47ed31d04fce9b9b596b965d56d3daecf9e69825
Farzin <tahayori@live.com>
2020-01-12 17:42:07 +0330

    Update new UI

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,13 @@
 var callback = function () {
-    document.querySelector('#header').onclick = function () { loadMenu(); return false; }
-function log (x){
-   // console.log(x)
-}
+    document.querySelector('#header').onclick = function () {
+
+
+        return false;
+    }
+
+    function log(x) {
+        console.log(x)
+    }
     // Handler when the DOM is fully loaded
     var socket = io();
     var clientinputprocess = false;
@@ -10,13 +15,16 @@ function log (x){
     var clientinputquestions = [];
     var clientinputanswer = {};
     var defform = '<input id="m" autofocus autocomplete="off" />';
-    var series = '';
-    loadMenu();
+    var series = [];
+
+    send('شروع');
+
     function msgProcessor(msg) {
         msg = JSON.parse(msg);
         log('Recieved', msg)
         return msg;
     }
+
     function emitter(room, msg) {
         var str = {
             date: new Date().getTime(),
@@ -24,67 +32,26 @@ function log (x){
         }
         socket.emit(room, JSON.stringify(str));
     }
-    function loadMenu() {
-        clientinputprocess = false;
-        clientinputanswer = {};
-        emitter('menu', 'show');
-        socket.on('menu', (msg) => {
-            var hh = window.innerHeight - 50;
-            document.querySelector('#header h1').innerHTML = 'موسسه زبان پارسا';
-            document.querySelector('ul#FirstMenu').innerHTML = '';
-            document.querySelector('ul#FirstMenu').setAttribute('style', '');
-            document.querySelector('ul#FirstMenu').setAttribute('style', 'height : ' + hh + 'px !important;')
-            document.querySelector('#messages').innerHTML = '';
-            document.querySelector('#messages').setAttribute('style', 'display: none;');
-            document.querySelector('form').setAttribute('style', 'display: none !important');
-            msg = msgProcessor(msg);
-            msg = msg.message;
-            var str = '';
-            msg.forEach((itm) => {
-                str = '<li><div class="photo"></div><div class="cntent"><h2>' + itm.title + '</h2><span class="tme">' + itm.time + '</span><p>' + itm.desc + '</p><span class="cntr"></span></div></li>'
-                document.querySelector('ul#FirstMenu').innerHTML += str;
 
-            });
-            document.querySelectorAll('ul#FirstMenu li').forEach((el) => { el.onclick = function () { loadChat(el.querySelector('h2').innerText); return false; } })
-
-        });
-    }
-
-    socket.on('loadChat', (msg) => {
-        msg = msgProcessor(msg);
-        msg = msg.message;
-        if (msg == 'ready') {
-            clientinputprocess = false;
-            send('شروع');
-        }
-    })
-    function loadChat(itm) {
-        document.querySelector('#header h1').innerHTML = itm;
-        document.querySelector('ul#FirstMenu').innerHTML = '';
-        document.querySelector('ul#FirstMenu').setAttribute('style', 'display: none !important');
-        document.querySelector('form').setAttribute('style', '');
-        document.querySelector('#messages').innerHTML = '';
-        document.querySelector('#messages').setAttribute('style', '');
-        series = [];
-        series.push(itm);
-        emitter('loadChat', itm)
-        return false;
-    }
     //var socket = io();
     document.forms[0].onsubmit = function () {
         var reqq = document.querySelector('#m').value;
         send(reqq);
         return false;
     };
+
     function send(requ) {
         window.scrollTo(0, document.body.scrollHeight);
-        if (requ == 'خروج') { loadMenu(); }
-        else {
+        if (requ == 'خروج') {
+            clientinputprocess = false;
+            series = [];
+            send('شروع');
+        } else {
             var date = new Date();
             date = formatter(date);
 
             setTimeout(() => {
-                document.querySelector('#messages').innerHTML += '<li class="req"><div class="you">شما</div><p>' + requ + '</p><div class="tme">' + date + '</div></li>';
+                document.querySelector('#messages').innerHTML += '<div class="msg"><div class="req"><div class="you">شما</div><p>' + requ + '</p><div class="tme">' + date + '</div></div><div>';
                 return false;
             }, 10)
 
@@ -101,20 +68,20 @@ function log (x){
                         inputProcessor(clientinputquestions[clientinputcntr].name, selectProcessor(clientinputquestions[clientinputcntr].items), date)
 
                     } else {
-                        inputProcessor(clientinputquestions[clientinputcntr].name + '<br><span class="note">' + clientinputquestions[clientinputcntr].desc + '</span>', defform, date)
+                        inputProcessor(clientinputquestions[clientinputcntr].name + '<br><span class="note">' + clientinputquestions[clientinputcntr].desc + '</span>', '', date)
                     }
                 } else {
                     if (requ == 'خیر') {
                         clientinputprocess = true;
                         clientinputcntr = 0;
                         log(clientinputquestions)
-                        inputProcessor(clientinputquestions[clientinputcntr].name, defform, date);
+                        inputProcessor(clientinputquestions[clientinputcntr].name, '', date);
                     } else if (requ == 'بله') {
                         clientinputprocess = false;
                         series.push(JSON.stringify(clientinputanswer))
                         series.unshift('#reg');
                         log(series.toString())
-                        emitter('room', series.toString());
+                        emitter('chat', series.toString());
                     } else {
                         var str11 = 'اطلاعات دریافتی به شرح زیر است آیا تایید می کنید؟<br>'
                         for (i in clientinputanswer) {
@@ -127,7 +94,7 @@ function log (x){
             } else {
                 series.push(requ);
                 log(series.toString())
-                emitter('room', series.toString());
+                emitter('chat', series.toString());
             }
             if (document.querySelector('#m')) document.querySelector('#m').value = '';
             window.scrollTo(0, document.body.scrollHeight);
@@ -137,38 +104,46 @@ function log (x){
 
     function inputProcessor(inputitem, resType, date) {
         setTimeout(() => {
-            var str = '<li class="res"><div class="you">ربات پارسا</div><p>' + inputitem + '<p><div class="tme">' + date + '</div></li>'
+            var str = '<div class="msg"><div class="res"><div class="you">ربات پارسا</div><p>' + inputitem + '<p><div class="tme">' + date + '</div></div>' + resType + '</div>'
             document.querySelector('#messages').innerHTML += str;
-            document.querySelector('form').innerHTML = resType;
-            document.querySelectorAll('li.link').forEach((el) => { el.onclick = function () { send(el.innerText); return false; } })
+            document.querySelectorAll('div.link').forEach((el) => {
+                el.onclick = function () {
+                    send(el.innerText);
+                    return false;
+                }
+            })
             window.scrollTo(0, document.body.scrollHeight);
             return false;
         }, 50);
         /*document.querySelector('input#m').focus();*/
 
     }
+
     function selectProcessor(msg) {
-        var str = '<ul>';
-        msg.forEach((x) => { str += '<li class="link">' + x + '</li>' });
-        str += '<li class="link">خروج</li>'
-        str += '</ul>'
+        var str = '<div class="actions">';
+        msg.forEach((x) => {
+            str += '<div class="link">' + x + '</div>'
+        });
+        str += '<div class="link">خروج</div>'
+        str += '</div>'
         return str;
     }
+
     function formatter(date) {
 
-        var y = _zero( date.getHours()) + ':' + _zero(date.getMinutes())
+        var y = _zero(date.getHours()) + ':' + _zero(date.getMinutes())
+
         function _zero(x) {
             if (x < 10) {
-                   x = '0' + x;
-                   return x;
-            }else{
+                x = '0' + x;
+                return x;
+            } else {
                 return x;
             }
         }
         return y;
     }
-    socket.on('room', function (msg) {
-
+    socket.on('chat', function (msg) {
         msg = msgProcessor(msg);
         var date = new Date(msg.date);
         msg = msg.message;
@@ -176,7 +151,6 @@ function log (x){
         log(msg);
         if (msg != undefined) {
             var tx = Object.keys(msg)[0];
-
             if (msg[tx].type) {
                 msg = msg[tx];
             } else if (msg.type) {
@@ -193,14 +167,14 @@ function log (x){
                     clientinputanswer = {}
                     clientinputcntr = 0;
                     clientinputquestions = msg.items;
-                    inputProcessor(tx, defform, date)
-                    inputProcessor(clientinputquestions[0].name + '<br><span class="note">' + clientinputquestions[0].desc + '</span>', defform, date)
+                    inputProcessor(tx, '', date)
+                    inputProcessor(clientinputquestions[0].name + '<br><span class="note">' + clientinputquestions[0].desc + '</span>', '', date)
                     break;
                 case 'text':
-                    inputProcessor(tx, defform, date)
+                    inputProcessor(tx, selectProcessor([]), date)
                     break;
                 default:
-                    inputProcessor(msg, defform, date)
+                    inputProcessor(msg, selectProcessor([]), date)
                     break;
             }
         }
